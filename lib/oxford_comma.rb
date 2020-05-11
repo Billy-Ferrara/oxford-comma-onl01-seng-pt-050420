46af00bc8f2706937839e0ec0bfbb5dfffcae767
Billy Ferrara <willfira717@gmail.com>
2020-05-11 19:29:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
     elsif array.size == 3
       return array.join(", ").insert(13, " and")
     else
-      array[0..-3].join(", ") + " and "
+      array[0..-1].join(", ") + " and "
     end
 end
