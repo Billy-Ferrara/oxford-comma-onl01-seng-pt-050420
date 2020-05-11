7202538f15e52dd79aa2f50d14d1f02fcdbdb50c
Billy Ferrara <willfira717@gmail.com>
2020-05-11 19:42:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,15 @@
 def oxford_comma(array)
   if array.size == 1
       return array[0]
+
     elsif array.size == 2
       return array.join(" and ")
-    elsif array.size == 3
-      return array.join(", ").insert(13, " and")
+
+    elsif array.size >= 3
+      return array[-1] = "and #{array[-1]}"
+      array.join(', ')
+
     else
-      array[0...-1].join(", ") list.last].join(", and ")
+      array[0..-1].join(", ").insert(32, " and")
     end
 end
