47c439cd71a72ec397da6e21ee69ffd67244e26a
Billy Ferrara <willfira717@gmail.com>
2020-05-11 19:03:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
-  if array.size == 2
-      return array.join("and ")
+  if array.size == 1
+      return array[0]
     elsif array.size == 3
       return array.join(", and")
     end
